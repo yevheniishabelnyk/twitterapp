3b854b88a081e353af5844daaae9ff0d5a4fb465
yevhenii.shabelnyk <yevhenii.shabelnyk@gmail.com>
2023-12-23 11:42:03 +0200

    Add mock data for tweets;

---
+++ src/mock.ts
@@ -0,0 +1,197 @@
+export const TWEETS = [
+  {
+    id: "1",
+    user: "Rodney Diaz",
+    userName: "@rodiaz",
+    avatar: require("./assets/avatar/user1.jpg"),
+
+    time: "1h",
+    message:
+      'Sometimes I see english words that make me smile - because they are clear "transpositions" of their latin/italian counterparts',
+  },
+  {
+    id: "2",
+    user: "Tammy Warren",
+    userName: "@tammy2",
+    avatar: null,
+
+    time: "2h",
+    message:
+      'Aqui les dejo el link a mi charla de ayer del #ReactWeekMedelin : "Some Video" youtube.com/watch?v=WC0Wkx #react #redux #RxJS',
+  },
+  {
+    id: "3",
+    user: "Jacqueline Hayes",
+    userName: "@jacqyes68",
+    avatar: require("./assets/avatar/user2.jpg"),
+
+    time: "1h",
+    message:
+      "Hard work is all you need if you're a hunter gatherer, but you're not.\n\nYouneed to convince people around you that whatever thing it is that you want to happen is something that they want to happen too.",
+  },
+  {
+    id: "4",
+    user: "Wyatt Fleming",
+    userName: "@attFleming",
+    avatar: require("./assets/avatar/user3.jpg"),
+
+    time: "32m",
+    message: "How do you learn sales?",
+  },
+  {
+    id: "5",
+    user: "Craig Griffin",
+    userName: "@cgriffin",
+    avatar: require("./assets/avatar/user1.jpg"),
+
+    time: "22h",
+    message: "I'm jumping on this AMA. Come keep me company.",
+  },
+  {
+    id: "6",
+    user: "Terry Davidson",
+    userName: "@davidson",
+    avatar: require("./assets/avatar/user3.jpg"),
+
+    time: "1h",
+    message: "I want @Dandremonteiro to pay you",
+  },
+  {
+    id: "7",
+    user: "Rodney Diaz",
+    userName: "@rodiaz",
+    avatar: require("./assets/avatar/user1.jpg"),
+
+    time: "1h",
+    message:
+      'Sometimes I see english words that make me smile - because they are clear "transpositions" of their latin/italian counterparts',
+  },
+  {
+    id: "8",
+    user: "Tammy Warren",
+    userName: "@tammy2",
+    avatar: null,
+
+    time: "2h",
+    message:
+      'Aqui les dejo el link a mi charla de ayer del #ReactWeekMedelin : "Some Video" youtube.com/watch?v=WC0Wkx #react #redux #RxJS',
+  },
+  {
+    id: "9",
+    user: "Jacqueline Hayes",
+    userName: "@jacqyes68",
+    avatar: require("./assets/avatar/user2.jpg"),
+
+    time: "1h",
+    message:
+      "Hard work is all you need if you're a hunter gatherer, but you're not.\n\nYouneed to convince people around you that whatever thing it is that you want to happen is something that they want to happen too.",
+  },
+  {
+    id: "10",
+    user: "Wyatt Fleming",
+    userName: "@attFleming",
+    avatar: require("./assets/avatar/user3.jpg"),
+
+    time: "32m",
+    message: "How do you learn sales?",
+  },
+];
+
+export const TWEETS_LOADED = [
+  {
+    id: "11",
+    user: "Rodney Diaz",
+    userName: "@rodiaz",
+    avatar: require("./assets/avatar/user1.jpg"),
+
+    time: "1h",
+    message:
+      'Sometimes I see english words that make me smile - because they are clear "transpositions" of their latin/italian counterparts',
+  },
+  {
+    id: "12",
+    user: "Tammy Warren",
+    userName: "@tammy2",
+    avatar: null,
+
+    time: "2h",
+    message:
+      'Aqui les dejo el link a mi charla de ayer del #ReactWeekMedelin : "Some Video" youtube.com/watch?v=WC0Wkx #react #redux #RxJS',
+  },
+  {
+    id: "13",
+    user: "Jacqueline Hayes",
+    userName: "@jacqyes68",
+    avatar: require("./assets/avatar/user2.jpg"),
+
+    time: "1h",
+    message:
+      "Hard work is all you need if you're a hunter gatherer, but you're not.\n\nYouneed to convince people around you that whatever thing it is that you want to happen is something that they want to happen too.",
+  },
+  {
+    id: "14",
+    user: "Wyatt Fleming",
+    userName: "@attFleming",
+    avatar: require("./assets/avatar/user3.jpg"),
+
+    time: "32m",
+    message: "How do you learn sales?",
+  },
+  {
+    id: "15",
+    user: "Craig Griffin",
+    userName: "@cgriffin",
+    avatar: require("./assets/avatar/user1.jpg"),
+
+    time: "22h",
+    message: "I'm jumping on this AMA. Come keep me company.",
+  },
+  {
+    id: "16",
+    user: "Terry Davidson",
+    userName: "@davidson",
+    avatar: require("./assets/avatar/user3.jpg"),
+
+    time: "1h",
+    message: "I want @Dandremonteiro to pay you",
+  },
+  {
+    id: "17",
+    user: "Rodney Diaz",
+    userName: "@rodiaz",
+    avatar: require("./assets/avatar/user1.jpg"),
+
+    time: "1h",
+    message:
+      'Sometimes I see english words that make me smile - because they are clear "transpositions" of their latin/italian counterparts',
+  },
+  {
+    id: "18",
+    user: "Tammy Warren",
+    userName: "@tammy2",
+    avatar: null,
+
+    time: "2h",
+    message:
+      'Aqui les dejo el link a mi charla de ayer del #ReactWeekMedelin : "Some Video" youtube.com/watch?v=WC0Wkx #react #redux #RxJS',
+  },
+  {
+    id: "19",
+    user: "Jacqueline Hayes",
+    userName: "@jacqyes68",
+    avatar: require("./assets/avatar/user2.jpg"),
+
+    time: "1h",
+    message:
+      "Hard work is all you need if you're a hunter gatherer, but you're not.\n\nYouneed to convince people around you that whatever thing it is that you want to happen is something that they want to happen too.",
+  },
+  {
+    id: "20",
+    user: "Wyatt Fleming",
+    userName: "@attFleming",
+    avatar: require("./assets/avatar/user3.jpg"),
+
+    time: "32m",
+    message: "How do you learn sales?",
+  },
+];
