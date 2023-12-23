dbbd55d661803844ab490f23645ceb92cdff057b
yevhenii.shabelnyk <yevhenii.shabelnyk@gmail.com>
2023-12-23 18:40:40 +0200

    Add Home screen;

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,69 @@
-import React from "react";
-import { Text } from "react-native";
+// @ts-nocheck
+import React, { useState, useEffect } from "react";
+import { FlatList } from "react-native";
+
+import { TWEETS, TWEETS_LOADED } from "../../mock";
+import Tweet from "../../components/Tweet";
+import TweetSkeleton from "../../components/TweetSkeleton";
+import { TweetProps, TweetItemType } from "../../models";
+import { MAX_TWEETS_LENGTH } from "../../utils/constants";
+
+import styles from "./styles";
+
+const arrayOfTenSkeletonItems = Array.from({ length: 10 }, (_, index) => index + 1);
 
 const Home: React.FC = () => {
-  return <Text>Home screen</Text>;
+  const [tweets, setTweets] = useState<TweetItemType[]>(TWEETS);
+  const [visibleTweets, setVisibleTweets] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    loadMoreTweets();
+  }, []);
+
+  const loadMoreTweets = () => {
+    if (!isLoading && tweets.length <= MAX_TWEETS_LENGTH) {
+      setIsLoading(true);
+
+      setTimeout(() => {
+        setTweets((prevTweets) => [...prevTweets, ...(prevTweets.length === 0 ? TWEETS : TWEETS_LOADED)]);
+
+        setVisibleTweets((prevVisibleTweets) => prevVisibleTweets + 10);
+
+        setIsLoading(false);
+      }, 2000); // Simulating a delay for loading
+    }
+  };
+
+  const renderItem = ({ item }: TweetProps) => {
+    return <Tweet item={item} />;
+  };
+
+  const renderListFooterComponent = () => {
+    return (
+      <>
+        {arrayOfTenSkeletonItems.map((item, _) => (
+          <TweetSkeleton key={item} />
+        ))}
+      </>
+    );
+  };
+
+  const data = tweets.slice(0, visibleTweets);
+
+  return (
+    <>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        style={styles.flatlistContainer}
+        onEndReached={loadMoreTweets}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={isLoading && renderListFooterComponent()}
+      />
+    </>
+  );
 };
 
 export default Home;
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,9 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({});
+import { COLORS } from "../../utils/constants";
+
+export default StyleSheet.create({
+  flatlistContainer: {
+    backgroundColor: COLORS.white,
+  },
+});
