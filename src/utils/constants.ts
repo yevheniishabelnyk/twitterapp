51820468859728c224dea06189eeee2a68a6314a
yevhenii.shabelnyk <yevhenii.shabelnyk@gmail.com>
2023-12-23 11:42:59 +0200

    Add constants;

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+  primary: "#1DA1F2",
+  secondary: "#14171A",
+  dark_gray: "#657786",
+  light_gray: "#AAB8C2",
+  exlight_gray: "#E1E8ED",
+  exexlight_gray: "#e9edf0",
+  white: "#FFF",
+};
+
+export const SCREEN_NAMES = {
+  HOME: "Home",
+  DETAIL: "DETAIL",
+  SETTINGS: "Settings",
+  LOGIN: "Login",
+};
+
+export const MAX_TWEETS_LENGTH = 20;
