c175f995b19ea797fa1aa71aaa9393b63bb640b8
yevhenii.shabelnyk <yevhenii.shabelnyk@gmail.com>
2023-12-23 11:00:20 +0200

    Add eslintrc.js and prettierrc configs;

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// .eslintrc.js
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+};
